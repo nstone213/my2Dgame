787afdcb67ac2381a94ba0b306e0a020ee4de3d9
nstone213 <nbaronstone@gmail.com>
2024-09-30 14:49:56 -0400

    Initialized GamePanel

--- main/Main.java
+++ main/Main.java
@@ -6,6 +6,12 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
