529474715e19eb33cf0d3f201eb0c9b7ead975ae
nstone213 <nbaronstone@gmail.com>
2024-10-04 14:01:40 -0400

    Added grass floor

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,8 +14,8 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
     final int scale = 3;
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
     int FPS = 60;
